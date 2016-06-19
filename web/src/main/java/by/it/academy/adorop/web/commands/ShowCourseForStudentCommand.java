b083958577ba896b6a002be50433481a27617c3f
adorop <adorop@ya.ru>
2016-06-19 14:03:48 +0300

    refactoring ShowCourseForStudentCommand

--- web/src/main/java/by/it/academy/adorop/web/commands/ShowCourseForStudentCommand.java
+++ web/src/main/java/by/it/academy/adorop/web/commands/ShowCourseForStudentCommand.java
@@ -1,54 +1,89 @@
 package by.it.academy.adorop.web.commands;
 
 import by.it.academy.adorop.model.Course;
-import by.it.academy.adorop.model.Mark;
 import by.it.academy.adorop.model.users.Student;
+import by.it.academy.adorop.service.api.CourseService;
 import by.it.academy.adorop.service.api.MarkService;
+import by.it.academy.adorop.service.api.StudentService;
 import by.it.academy.adorop.service.exceptions.ServiceException;
+import by.it.academy.adorop.service.implementations.StudentServiceImpl;
 import by.it.academy.adorop.web.utils.Dispatcher;
-import by.it.academy.adorop.web.utils.IdValidator;
 import by.it.academy.adorop.web.utils.PathBuilder;
+import by.it.academy.adorop.web.utils.RequestParamValidator;
 import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
+
+import static by.it.academy.adorop.web.utils.Constants.*;
 
 class ShowCourseForStudentCommand extends BasicShowCourseCommand {
 
+    private final StudentService studentService;
 
-    public ShowCourseForStudentCommand(HttpServletRequest request, IdValidator<Course> idValidator, MarkService markService) {
-        super(request, idValidator, markService);
+    ShowCourseForStudentCommand(HttpServletRequest request) {
+        super(request);
+        studentService = StudentServiceImpl.getInstance();
+    }
+
+    ShowCourseForStudentCommand(HttpServletRequest request, MarkService markService, CourseService courseService, StudentService studentService) {
+        super(request, markService, courseService);
+        this.studentService = studentService;
     }
 
     @Override
-    protected boolean requestIsValid() {
-        return false;
+    protected boolean requestIsValid() throws ServiceException {
+        return RequestParamValidator.isValidId(courseIdParameter, courseService);
     }
 
     @Override
-    protected void setContent() {
+    protected void setContent() throws ServiceException {
+        Course course = getCourse();
+        Student student = getStudent();
+        boolean isCourseListener = studentService.isCourseListener(student, course);
+        setCourse(course);
+        setIsCourseListener(isCourseListener);
+        setMark(course, student, isCourseListener);
+        setPathToProcessRegistrationForTheCourse();
+    }
 
+    private Student getStudent() {
+        return (Student) request.getSession().getAttribute("student");
+    }
+
+    private Course getCourse() throws ServiceException {
+        return courseService.find(Long.valueOf(courseIdParameter));
+    }
+
+    private void setMark(Course course, Student student, boolean isCourseListener) throws ServiceException {
+        if (isCourseListener) {
+          request.setAttribute("mark", markService.getByStudentAndCourse(student, course));
+        }
+    }
+
+    private void setIsCourseListener(boolean isCourseListener) {
+        request.setAttribute("isCourseListener", isCourseListener);
+    }
+
+    private void setCourse(Course course) {
+        request.setAttribute("course", course);
+    }
+
+    private void setPathToProcessRegistrationForTheCourse() {
+        String path = PathBuilder.buildPath(request, OPERATION_REGISTER_FOR_THE_COURSE, "courseId", courseIdParameter);
+        request.setAttribute("pathToProcessRegistrationForTheCourse", path);
     }
 
     @Override
-    protected void move(HttpServletResponse response) {
-
-    }
-
-    @Override
-    protected void setExplainingMessage() {
-
-    }
-
-    @Override
-    protected void sendToRelevantPage(HttpServletResponse response) {
-
+    protected void move(HttpServletResponse response) throws ServletException, IOException {
+        Dispatcher.forward(COURSE_FOR_STUDENTS_PAGE, request, response);
     }
 
     @Override
     protected Logger getLogger() {
-        return null;
+        return Logger.getLogger(ShowCourseForStudentCommand.class);
     }
 }
