8bf0ce78c4ae1c610c5a064e04afc441fb2bd0db
adorop <adorop@ya.ru>
2016-07-05 18:34:21 +0300

    add course feature

--- web/src/main/java/by/it/academy/adorop/web/controllers/TeacherController.java
+++ web/src/main/java/by/it/academy/adorop/web/controllers/TeacherController.java
@@ -1,24 +1,33 @@
 package by.it.academy.adorop.web.controllers;
 
+import by.it.academy.adorop.model.Course;
 import by.it.academy.adorop.model.users.Teacher;
 import by.it.academy.adorop.service.api.CourseService;
+import by.it.academy.adorop.service.api.TeacherService;
 import by.it.academy.adorop.web.utils.pagination.PaginationContentPutter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/teachers")
 public class TeacherController {
 
     private final CourseService courseService;
+    private final TeacherService teacherService;
 
     @Autowired
-    public TeacherController(CourseService courseService) {
+    public TeacherController(CourseService courseService, TeacherService teacherService) {
         this.courseService = courseService;
+        this.teacherService = teacherService;
     }
 
     @RequestMapping
@@ -28,7 +37,20 @@ public class TeacherController {
     }
 
     @RequestMapping("/add")
-    public String addCourse() {
-        return "";
+    public String addCourse(Model model, @ModelAttribute Course course) {
+        model.addAttribute("course", course);
+        return "course/add";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String saveNewCourse(@Valid Course course,
+                                BindingResult bindingResult,
+                                Model model,
+                                @AuthenticationPrincipal Teacher teacher) {
+        if (bindingResult.hasErrors()) {
+            return addCourse(model, course);
+        }
+        teacherService.addCourse(teacher, course);
+        return "redirect:/teachers";
     }
 }
--- web/src/main/java/by/it/academy/adorop/web/utils/RequestParamValidator.java
+++ web/src/main/java/by/it/academy/adorop/web/utils/RequestParamValidator.java
@@ -7,15 +7,6 @@ public class RequestParamValidator {
     private RequestParamValidator() {
     }
 
-//    public static boolean hasEmpty(String... params) {
-//        for (String param : params) {
-//            if (param == null || param.equals("")) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-    
     public static boolean isEmpty(String param) {
         return param == null || param.equals("");
     }
@@ -23,13 +14,4 @@ public class RequestParamValidator {
     public static boolean isPositiveInt(String param) {
         return !isEmpty(param) && param.matches("[0-9]+");
     }
-
-//    public static boolean isNumberBetweenZeroAndTen(String param) {
-//        return !isEmpty(param) && param.matches("[0-9]|(10)");
-//    }
-//
-//    @SuppressWarnings("unchecked")
-//    public static boolean isValidId(String idParameter, Service service) {
-//        return isPositiveInt(idParameter) && service.find(Long.valueOf(idParameter)) != null;
-//    }
 }
