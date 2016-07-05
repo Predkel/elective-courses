21079bb8d2dc4445c7fb47bdd296cec73cdadd3c
adorop <adorop@ya.ru>
2016-07-05 17:42:31 +0300

    main teachers

--- web/src/main/java/by/it/academy/adorop/web/controllers/TeacherController.java
+++ web/src/main/java/by/it/academy/adorop/web/controllers/TeacherController.java
@@ -1,15 +1,29 @@
 package by.it.academy.adorop.web.controllers;
 
 import by.it.academy.adorop.model.users.Teacher;
+import by.it.academy.adorop.service.api.CourseService;
+import by.it.academy.adorop.web.utils.pagination.PaginationContentPutter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/teachers")
 public class TeacherController {
 
-    public String showCourses() {
+    private final CourseService courseService;
+
+    @Autowired
+    public TeacherController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @RequestMapping
+    public String showCourses(HttpServletRequest request) {
+        PaginationContentPutter.putPaginationContent(request, courseService, "courses");
         return "main/teachers";
     }
 }
--- web/src/test/java/by/it/academy/adorop/web/controllers/TeacherControllerTest.java
+++ web/src/test/java/by/it/academy/adorop/web/controllers/TeacherControllerTest.java
@@ -1,23 +1,47 @@
 package by.it.academy.adorop.web.controllers;
 
+import by.it.academy.adorop.service.api.CourseService;
+import by.it.academy.adorop.web.utils.pagination.PaginationContentPutter;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import javax.servlet.http.HttpServletRequest;
 
 import static org.junit.Assert.*;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(PaginationContentPutter.class)
 public class TeacherControllerTest {
 
     private TeacherController controller;
 
+    @Mock
+    private HttpServletRequest request;
+    @Mock
+    private CourseService courseService;
+
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        controller = new TeacherController();
+        PowerMockito.mockStatic(PaginationContentPutter.class);
+        controller = new TeacherController(courseService);
     }
 
     @Test
-    public void testMain() throws Exception {
-        assertEquals("main/teachers", controller.showCourses());
+    public void testShowCourses() throws Exception {
+        assertEquals("main/teachers", controller.showCourses(request));
+    }
+
+    @Test
+    public void showCoursesShouldPutPaginationContent() throws Exception {
+        controller.showCourses(request);
+        PowerMockito.verifyStatic();
+        PaginationContentPutter.putPaginationContent(request, courseService, "courses");
     }
 }
