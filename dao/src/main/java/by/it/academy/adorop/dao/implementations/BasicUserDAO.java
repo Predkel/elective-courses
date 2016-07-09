bc24f2ce7da6788c8c4a8e82669cf75a2643d1b8
adorop <adorop@ya.ru>
2016-07-10 02:06:48 +0300

    small refactoring in basic user service

--- dao/src/main/java/by/it/academy/adorop/dao/implementations/BasicUserDAO.java
+++ dao/src/main/java/by/it/academy/adorop/dao/implementations/BasicUserDAO.java
@@ -2,7 +2,6 @@ package by.it.academy.adorop.dao.implementations;
 
 import by.it.academy.adorop.dao.api.UserDAO;
 import by.it.academy.adorop.model.users.User;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 
@@ -16,15 +15,17 @@ public abstract class BasicUserDAO<T extends User> extends BasicDAO<T, Long> imp
     @Override
     @SuppressWarnings("unchecked")
     public T getByDocumentId(String documentId) {
-        return (T) currentSession().createCriteria(getPersistedClass())
-                .add(Restrictions.eq("documentId", documentId))
-                .uniqueResult();
+        return (T) getBy(documentId, getPersistedClass());
     }
 
     @Override
     public boolean isAlreadyExists(String documentId) {
-        return currentSession().createCriteria(User.class)
+        return getBy(documentId, User.class) != null;
+    }
+
+    private Object getBy(String documentId, Class userType) {
+        return currentSession().createCriteria(userType)
                 .add(Restrictions.eq("documentId", documentId))
-                .uniqueResult() != null;
+                .uniqueResult();
     }
 }
