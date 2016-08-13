ec6b274bbf8a9051fd7dc9b4ef51723a250d31f8
adorop <adorop@ya.ru>
2016-08-13 12:58:22 +0300

    providing debug information about dao & service methods with -Dspring.profiles.active=debug

---
+++ service/src/main/java/by/it/academy/adorop/service/config/ProfilingBeanPostProcessor.java
@@ -0,0 +1,49 @@
+package by.it.academy.adorop.service.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ProfilingBeanPostProcessor implements BeanPostProcessor, Ordered {
+
+    private Set<String> serviceBeanNames = new HashSet<>();
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        Class<?> beanClass = bean.getClass();
+        if (beanClass.isAnnotationPresent(Service.class)) {
+            serviceBeanNames.add(beanName);
+        }
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        Class<?> beanClass = bean.getClass();
+        if (serviceBeanNames.contains(beanName)) {
+            return Proxy.newProxyInstance(beanClass.getClassLoader(), beanClass.getInterfaces(), new InvocationHandler() {
+                @Override
+                public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                    long before = System.nanoTime();
+                    Object retVal = method.invoke(bean, args);
+                    long invocationTime = System.nanoTime() - before;
+                    System.out.println(method.getName() + ": " + invocationTime);
+                    return retVal;
+                }
+            });
+        }
+        return bean;
+    }
+
+    @Override
+    public int getOrder() {
+        return LOWEST_PRECEDENCE;
+    }
+}
