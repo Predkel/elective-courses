f0b854329ec6e75359a54e5383098e67293fe611
adorop <adorop@ya.ru>
2016-09-11 13:59:01 +0300

    implementing server-side exception handling

--- web/src/main/java/by/it/academy/adorop/web/infrastructure/ExceptionHandlerAdvice.java
+++ web/src/main/java/by/it/academy/adorop/web/infrastructure/ExceptionHandlerAdvice.java
@@ -3,6 +3,8 @@ package by.it.academy.adorop.web.infrastructure;
 import by.it.academy.adorop.service.exceptions.ServiceException;
 import org.apache.log4j.Logger;
 import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,35 +12,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 
-//@ControllerAdvice
+@ControllerAdvice
 public class ExceptionHandlerAdvice {
 
-    public static final String BAD_REQUEST_PAGE = "badRequest";
     private final Logger logger = Logger.getLogger(ExceptionHandlerAdvice.class);
 
     @ExceptionHandler(ServiceException.class)
-    public String catchServiceException(Exception e) {
-        logger.error(e);
-        return "error";
-    }
-
-    @ExceptionHandler(TypeMismatchException.class)
-    public String catchTypeMismatchException(Exception e, Model model, HttpServletRequest request) {
-        return sendToBadRequestPage(model, request);
-    }
-
-    @ExceptionHandler(AccessDeniedException.class)
-    public String catchAccessDeniedException(Exception e, Model model, HttpServletRequest request) {
-        return sendToBadRequestPage(model, request);
-    }
-
-    private String sendToBadRequestPage(Model model, HttpServletRequest request) {
-        String pathToMain = definePathToMain(request);
-        model.addAttribute("pathToMain", pathToMain);
-        return BAD_REQUEST_PAGE;
-    }
-
-    private String definePathToMain(HttpServletRequest request) {
-        return request.getServletPath().contains("teachers") ? "/teachers" : "/students";
+    public ResponseEntity catchServiceException(ServiceException e) {
+        logger.error(e.toString());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
     }
 }
